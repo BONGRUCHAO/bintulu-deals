b85af3d6b9600a0eb6a68346b63e65b0e587cdd6
BONGRUCHAO <87805850+BONGRUCHAO@users.noreply.github.com>
2026-06-18 23:26:19 +0800

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('push', function(event){
+
+  const data = event.data.json();
+
+  event.waitUntil(
+      self.registration.showNotification(
+          data.title,
+          {
+              body:data.body
+          }
+      )
+  );
+
+});
