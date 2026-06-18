1ffe403beae26b2beb245afb16038ac6ecc0788e
BONGRUCHAO <87805850+BONGRUCHAO@users.noreply.github.com>
2026-06-19 00:36:09 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,21 +1,9 @@
 import json
-from datetime import datetime
 
-def run():
+deals = [
+    {"title": "TEST Econsave Sale"},
+    {"title": "TEST Emart Promo"}
+]
 
-    deals = [
-        {
-            "title": "Econsave Weekend Sale - RM1 Deals",
-            "time": str(datetime.now())
-        },
-        {
-            "title": "Emart Bintulu Fresh Discount Up To 30%",
-            "time": str(datetime.now())
-        }
-    ]
-
-    with open("data.json", "w", encoding="utf-8") as f:
-        json.dump(deals, f, ensure_ascii=False, indent=2)
-
-if __name__ == "__main__":
-    run()
+with open("data.json", "w", encoding="utf-8") as f:
+    json.dump(deals, f, ensure_ascii=False, indent=2)
