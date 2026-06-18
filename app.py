31c085fa4516f86e03b47328a09fb630bda785a1
BONGRUCHAO <87805850+BONGRUCHAO@users.noreply.github.com>
2026-06-19 00:04:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
-
-
 
 @app.route("/")
 def index():
@@ -16,21 +13,16 @@ def index():
 
     return render_template("index.html", deals=data)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
 
 @app.route("/subscribe", methods=["POST"])
 def subscribe():
-
     sub = request.json
 
-    with open(
-        "subs.json",
-        "a",
-        encoding="utf-8"
-    ) as f:
-
-        f.write(json.dumps(sub))
-        f.write("\n")
+    with open("subs.json", "a", encoding="utf-8") as f:
+        f.write(json.dumps(sub) + "\n")
 
     return {"ok": True}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
