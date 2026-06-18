057dc346b6e544de83a444296eec8d228a38d33d
BONGRUCHAO <87805850+BONGRUCHAO@users.noreply.github.com>
2026-06-18 23:27:58 +0800

    Update send_push.py

--- send_push.py
+++ send_push.py
@@ -1,1 +1,34 @@
+import json
+from pywebpush import webpush
 
+PUBLIC_KEY = "你的公钥"
+PRIVATE_KEY = "你的私钥"
+
+
+def send_all(title, body):
+
+    try:
+        with open(
+            "subs.json",
+            "r",
+            encoding="utf-8"
+        ) as f:
+
+            for line in f:
+
+                sub = json.loads(line)
+
+                webpush(
+                    subscription_info=sub,
+                    data=json.dumps({
+                        "title": title,
+                        "body": body
+                    }),
+                    vapid_private_key=PRIVATE_KEY,
+                    vapid_claims={
+                        "sub":"mailto:test@test.com"
+                    }
+                )
+
+    except:
+        pass
